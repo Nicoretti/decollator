cb620bef8d866694e0a35e5cdf86c900bb7d466e
Nicola Coretti <nico.coretti@gmail.com>
2022-04-26 22:03:20 +0200

    Initial commit

---
+++ test/unit/decollator/cli_test.py
@@ -0,0 +1,9 @@
+import pytest
+from decollator.cli import SUCCESS, FAILURE
+
+
+def test_cli_constant_success():
+    assert SUCCESS == 0
+
+def test_cli_constant_failure():
+    assert FAILURE == -1
