a0946e2f45f448b117df019417c48132b290736d
Nicola Coretti <nico.coretti@gmail.com>
2022-04-26 22:18:07 +0200

    Run code formatter on code base

--- test/unit/decollator/cli_test.py
+++ test/unit/decollator/cli_test.py
@@ -1,9 +1,11 @@
 import pytest
-from decollator.cli import SUCCESS, FAILURE
+
+from decollator.cli import FAILURE, SUCCESS
 
 
 def test_cli_constant_success():
     assert SUCCESS == 0
+
 
 def test_cli_constant_failure():
     assert FAILURE == -1
